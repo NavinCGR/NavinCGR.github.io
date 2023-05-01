e60db309148d386fe75605622ea4360d626cb0fd
NavinCGR <115581815+NavinCGR@users.noreply.github.com>
2023-05-01 17:47:46 +0530

    Add alert message

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ const button = document.querySelector("button");
 const display = document.querySelector("span");
 
 button.addEventListener('click', () => {
+    alert("Btn clicked");
     display.textContent = "Hey";
 })
